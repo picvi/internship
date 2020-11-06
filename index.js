664efdbb0cf2fa26567d753667bf9d8762e0b7af
picvi <viki.galaburda@gmail.com>
2020-11-06 16:09:26 +0300

    fixed sorting within one type of data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let data = ['sdf', 35, 'a', true, 'nsu', 453, 3, null, {a: 123}, undefined]
+let data = ['cd', 35, 'a', true, 'bss', 453, 3, null, {a: 123}, undefined]
 let method = ['boolean', 'number', 'string', 'undefined', 'object', 'null']
 
 function sort(arrayWithData, sortMethod) {
   return arrayWithData.sort((a, b) => {
     if (sortMethod.indexOf(typeof a) === sortMethod.indexOf(typeof b)) {
-      return a - b;
+      return a > b ? 1 : -1;
     }
     return sortMethod.indexOf(typeof a) > sortMethod.indexOf(typeof b) ? 1 : -1;
   })
