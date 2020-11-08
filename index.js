15ec7ec0ae8b5fc797bb6a96d430a1793a37a54f
picvi <69312447+picvi@users.noreply.github.com>
2020-11-08 18:53:55 +0300

    changed sorting function

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ let data = ['cd', 35, 'a', true, 'bss', 453, 3, null, {a: 123}, undefined]
 let method = ['boolean', 'number', 'string', 'undefined', 'object', 'null']
 
 function sort(arrayWithData, sortMethod) {
-  for (let i = 0; i< arrayWithData.length; i++) {
-    arrayWithData[i] === undefined ? arrayWithData[i] = "" : arrayWithData[i]
-  }
+  const undef = {};
 
-  arrayWithData.sort((a, b) => {
-    if (sortMethod.indexOf(typeof a) === sortMethod.indexOf(typeof b)) {
-      return a > b ? 1 : -1;
-    }
-    if (a === "") {
-      return sortMethod.indexOf("undefined") > sortMethod.indexOf(typeof b) ? 1 : -1
-    }
-
-    return sortMethod.indexOf(typeof a) > sortMethod.indexOf(typeof b) ? 1 : -1;
-  })
-
-  for (let i = 0; i< arrayWithData.length; i++) {
-    arrayWithData[i] === "" ? arrayWithData[i] = undefined : arrayWithData[i]
-  }
-  return arrayWithData;
+    const newArray = arrayWithData.map(element => {
+      return element === null ?  'null' : element === undefined ? element = undef : element;
+    })
+    .sort((a, b) => {
+      if (a === 'null') {
+        return sortMethod.indexOf('null') > sortMethod.indexOf(typeof b) ? 1 : -1;
+      }
+      if (a === undef) {
+        return sortMethod.indexOf('undefined') > sortMethod.indexOf(typeof b) ? 1 : -1;
+      }
+      return sortMethod.indexOf(typeof a) > sortMethod.indexOf(typeof b) ? 1 : -1;
+    })
+    .map(element => {
+      return element === 'null' ? null  : element === undef ? element = undefined : element;
+    })
+    
+  return newArray;
 }
 
 const result = sort(data, method);
